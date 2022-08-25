9d3f373607501f7a45bf456c35ae9c53ac424b64
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2022-08-25 10:26:31 -0300

    25/05/2022

--- biseccion/src/biseccion.java
+++ biseccion/src/biseccion.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class biseccion {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! asdf");
+        //Invoca módulos para realizar biseccion
+        Scanner sc = new Scanner(System.in);
+        double a, b, r;
+        System.out.print("Ingrese el valor de a: ");
+        a = sc.nextDouble();
+        
+
+
     }
+
+    public static double funcion(double x) {
+        //retorna el calculo con algun x ingresado
+        double result;
+        result = 2*Math.sin(x+1); //Ingresar funcion a evaluar en esta linea
+        return result;
+    }
+
+     
+
 }
