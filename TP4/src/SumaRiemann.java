a2883b4b461a201e539b54dda257e21fd3baea4d
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2022-11-03 18:01:20 -0300

    fix

--- TP4/src/SumaRiemann.java
+++ TP4/src/SumaRiemann.java
@@ -8,8 +8,9 @@ public class SumaRiemann {
         /*Algoritmo principal, aproxima volumen de f(x,y) con sumatoria de Riemannm, invoca la sumatoria 
         con una region S=[x0,x1]X[y0,y1] y un limite de sumatoria m=n por parametro*/
         double volumenAprox;
-        double x0 = 0, x1 = 2, y0 = 0, y1 = 1, m = 2; //Reemplazar region y m segun se necesite aproximar
-        volumenAprox = Riemann(x0,x1,y0,y1,m); 
+        double x0 = 0, x1 = 2, y0 = 0, y1 = 1; //Reemplazar region y m segun se necesite aproximar
+        int m=4, n=4;
+        volumenAprox = Riemann(x0,x1,y0,y1,m,n); 
         System.out.println("-------------------------------------------------------------------------------------------");
         System.out.print("Volumen aproximado por Sumatoria de Riemann (S=["+x0+","+x1+"]x["+y0+","+y1+"] y m=n="+m+"): ");
         System.out.println(volumenAprox+" u^3");
@@ -22,10 +23,11 @@ public class SumaRiemann {
      * @param x1 //Extremo de integración 
      * @param y0 //Extremo de integración 
      * @param y1 //Extremo de integración 
-     * @param m //Limite de las sumatorias (m=n)
+     * @param m //Limite de sumatoria
+     * @param n //Limite de sumatoria 
      * @return
      */
-    public static double Riemann(double x0, double x1, double y0, double y1, double m) {
+    public static double Riemann(double x0, double x1, double y0, double y1, int m, int n) {
         double ladoX = x1-x0; //Medida del lado X
         double ladoY = y1-y0; //Medida del lado Y
         double x, y; //Valores donde se evaluará la funcion
@@ -34,13 +36,13 @@ public class SumaRiemann {
         double suma = 0;
         
         dx = ladoX/m; //Divide el lado en m porciones
-        dy = ladoY/m; //Divide el lado en m porciones
+        dy = ladoY/n; //Divide el lado en n porciones
         dA = dx*dy; //Area de los cuadrados
         x = x0+dx; //Calcula desde x0+dx
         y = y0+dy; //Calcula desde y0+dy
         
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = 1; j <= n; j++) {
                 //Calcula y suma f(x,y), luego aumenta y en dy
                 suma = f(x,y)+suma;
                 y = y+dy;
